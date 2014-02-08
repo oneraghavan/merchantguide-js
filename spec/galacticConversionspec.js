fd11f7c5b4c7a1ff80269721736f368eab0076ff
Raghavan <oneraghavan@gmail.com>
2014-02-08 13:13:57 +0530

    refactored galacticConversionspec

--- spec/galacticConversionspec.js
+++ spec/galacticConversionspec.js
@@ -1,16 +1,21 @@
 describe("A suite", function() {
+  var galacticMapping;
+
+  beforeEach(function(){
+    galacticMapping = {'pish': 10, 'glob': 1, 'prok': 5, 'tegj': 50};
+  })
+
   it("Should return valid value for a given galactic number", function() {
-  	var galacticMapping = {'pish': 10, 'glob': 1, 'prok': 5, 'tegj': 50};
   	var galacticConversion = require("../galacticConversion.js");
   	var gConversion = new galacticConversion(galacticMapping);
   	expect(gConversion.getValuesFromGalactic(['pish','tegj','glob'])).toBe(41)
-})});
+})
 
-describe("A suite", function() {
   it("Should return invalid value for a given galactic number", function() {
-  	var galacticMapping = {'pish': 10, 'glob': 1, 'prok': 5, 'tegj': 50};
   	var galacticConversion = require("../galacticConversion.js");
   	var gConversion = new galacticConversion(galacticMapping);
   	expect(gConversion.getValuesFromGalactic(['pish','tegj1','glob1'])).toBe(-1)
-})});
+})
+  
+});
 
